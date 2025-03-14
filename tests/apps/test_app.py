c2c3aa29ea63ca34ec594af06c94cedb9a06d3ae
GinzburgLev <ginzburglev95@gmail.com>
2025-03-14 09:40:07 +0100

    fix test

--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -1,5 +1,5 @@
 def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
-    from pynxtools_automation.apps import app_entry_point
+    from pynxtools_camels.apps import app_entry_point
 
     assert app_entry_point.app.label == 'Automation'
